a50fa05745f484fafb13dbe089728e69f2e1038b
rnazb <r.nazbat@gmail.com>
2021-09-07 17:28:33 +0800

    Add slug-specific blog pages

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,76 @@
 import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Image from 'next/image'
+import ReactMarkdown from 'react-markdown'
+import PageCard from '../../components/UI/PageCard'
+import { Container } from 'react-bootstrap'
+
+import { fetchAPI } from '../../lib/api'
+import { getStrapiMedia } from '../../lib/media'
+import Seo from '../../components/Seo'
 
 import styles from './index.module.css'
 
-export default function Article() {
+export default function Article({ article }) {
   const router = useRouter()
+  if (!router.isFallback && !article?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  const imageUrl = getStrapiMedia(article.image)
+
+  const seo = {
+    metaTitle: article.title,
+    metaDescription: article.description,
+    shareImage: article.image,
+    article: true,
+  }
 
   return (
-    <div className={styles.page}>
-      <h1>Blog Post Page: {router.query.blogId}</h1>
-    </div>
+    <>
+      <Seo seo={seo} />
+      <Container className="mb-4">
+        <PageCard className={styles['article']}>
+          <article>
+            <div className={styles['article-image']}>
+              <Image
+                unoptimized={true}
+                width={2000}
+                height={1000}
+                objectFit="contain"
+                alt={`Cover Image for ${article.title}`}
+                src={imageUrl}
+              />
+            </div>
+            <h1 className={styles['article-title']}>{article.title}</h1>
+            <div className={styles['article-content']}>
+              <ReactMarkdown>{article.content}</ReactMarkdown>
+            </div>
+          </article>
+        </PageCard>
+      </Container>
+    </>
   )
+}
+
+export async function getStaticPaths() {
+  const articles = await fetchAPI("/articles")
+
+  return {
+    paths: articles.map((article) => ({
+      params: {
+        slug: article.slug,
+      },
+    })),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params, preview = null }) {
+  const articles = await fetchAPI(`/articles?slug=${params.slug}`)
+
+  return {
+    props: { article: articles[0] },
+    revalidate: 1,
+  };
 }

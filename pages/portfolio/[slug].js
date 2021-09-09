b15fd4bdcad6bb3e8d4cc67a6495fe4b416f8d98
rnazb <r.nazbat@gmail.com>
2021-09-09 15:44:38 +0800

    Redeploy

--- pages/portfolio/[slug].js
+++ pages/portfolio/[slug].js
@@ -1,5 +1,5 @@
-export default function PortfolioItem() {
+export default function PortfolioItem({ item }) {
   return (
-    <h1>Portfolio Item</h1>
+    <h1>Portfolio Item: {item.Title}</h1>
   )
 }

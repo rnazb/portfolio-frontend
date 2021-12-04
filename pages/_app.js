f611818f532cacd195953cdd5972cf5d088fa45d
Rey Nazareno <r.nazbat@gmail.com>
2021-12-04 17:38:04 +0800

    add google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,34 @@
-import App from 'next/app'
-import { createContext } from 'react'
-import { getStrapiMedia } from '../lib/media'
-import { fetchAPI } from '../lib/api'
+import App from "next/app"
+import { useRouter } from "next/router"
+import { createContext, useEffect } from "react"
+import { getStrapiMedia } from "../lib/media"
+import { fetchAPI } from "../lib/api"
 
-import Layout from '../components/Layout/Layout'
-import Footer from '../components/Layout/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../styles/globals.css'
+import Layout from "../components/Layout/Layout"
+import Footer from "../components/Layout/Footer"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/globals.css"
 
-export const GlobalContext = createContext({});
+import * as ga from "../lib/ga"
+
+export const GlobalContext = createContext({})
 
 function MyApp({ Component, pageProps }) {
-  const { global } = pageProps;
+  const { global } = pageProps
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
@@ -31,11 +48,11 @@ function MyApp({ Component, pageProps }) {
 // https://github.com/vercel/next.js/discussions/10949
 MyApp.getInitialProps = async (ctx) => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(ctx);
+  const appProps = await App.getInitialProps(ctx)
   // Fetch global site settings from Strapi
-  const global = await fetchAPI("/global");
+  const global = await fetchAPI("/global")
   // Pass the data to our page via props
-  return { ...appProps, pageProps: { global } };
-};
+  return { ...appProps, pageProps: { global } }
+}
 
-export default MyApp;
+export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -22,6 +22,23 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap"
             rel="stylesheet"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
